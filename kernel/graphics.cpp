33b5b16ddc24a71b08f4ae3b8f32f2ab0b385fcd
song-dela-moon <song.dela.moon@gmail.com>
2026-02-05 11:18:45 +0900

    [KosmOS_day05b] Add ASCII loop in Main for testing
    Changelog
    ======
    - Add loop in KernelMain to call WriteAscii from '!' to '~'.
    - Note: Output is still only 'A' due to WriteAscii restriction.
    - Prepare build environment for future font assets.
    
    Testing
    ======
    - Verified: Row of 'A' characters appearing on screen.
    
    Technical Insights
    ======
    - Testing character spacing (8 pixels) before full font integration.

---
+++ kernel/graphics.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file graphics.cpp
+ *
+ * a collection of image-drawing related programs.
+ */
+
+// #@@range_begin(pixel_writer_impl)
+#include "graphics.hpp"
+
+void RGBResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& c) {
+  auto p = PixelAt(x, y);
+  p[0] = c.r;
+  p[1] = c.g;
+  p[2] = c.b;
+}
+// #@@range_end(pixel_writer_impl)
+
+void BGRResv8BitPerColorPixelWriter::Write(int x, int y, const PixelColor& c) {
+  auto p = PixelAt(x, y);
+  p[0] = c.b;
+  p[1] = c.g;
+  p[2] = c.r;
+}
+
