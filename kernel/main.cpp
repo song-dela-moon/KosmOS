6ba123f1ede47303c8dbda20ae033a56d64e5d1b
song-dela-moon <song.dela.moon@gmail.com>
2026-02-07 13:06:25 +0900

    [KosmOS_day07b] Add FIFO queue for event loop
    Changelog
    ======
    - Implement ArrayQueue template (Ring Buffer) class.
    - Update IntHandlerXHCI to push events to the queue.
    - Implement EOI notification for Local APIC.
    - Refactor main to process events using a while loop.
    - Use hlt instruction to enter low-power state when idle.
    
    Testing
    ======
    - Verified: Smooth mouse processing via producer-consumer model.
    
    Technical Insights
    ======
    - Decoupling interrupt handling from main execution flow.

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -21,6 +21,7 @@
 #include "usb/xhci/trb.hpp"
 #include "interrupt.hpp"
 #include "asmfunc.h"
+#include "queue.hpp"
 // #@@range_end(includes)
 
 const PixelColor kDesktopBGColor{42, 42, 42};
@@ -81,17 +82,22 @@ void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
 }
 // #@@range_end(switch_echi2xhci)
 
-// #@@range_begin(xhci_handler)
 usb::xhci::Controller* xhc;
 
+// #@@range_begin(queue_message)
+struct Message {
+  enum Type {
+    kInterruptXHCI,
+  } type;
+};
+
+ArrayQueue<Message>* main_queue;
+// #@@range_end(queue_message)
+
+// #@@range_begin(xhci_handler)
 __attribute__((interrupt))
 void IntHandlerXHCI(InterruptFrame* frame) {
-  while (xhc->PrimaryEventRing()->HasFront()) {
-    if (auto err = ProcessEvent(*xhc)) {
-      Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
-          err.Name(), err.File(), err.Line());
-    }
-  }
+  main_queue->Push(Message{Message::kInterruptXHCI});
   NotifyEndOfInterrupt();
 }
 // #@@range_end(xhci_handler)
@@ -142,6 +148,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     pixel_writer, kDesktopBGColor, {300, 200}
   };
   // #@@range_end(new_mouse_cursor)
+
+  std::array<Message, 32> main_queue_data;
+  ArrayQueue<Message> main_queue{main_queue_data};
+  ::main_queue = &main_queue;
 
   auto err = pci::ScanAllBus();
   Log(kDebug, "ScanAllBus: %s\n", err.Name());
@@ -233,7 +243,34 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   }
   // #@@range_end(configure_port)
 
-  while (1) __asm__("hlt");
+  // #@@range_begin(event_loop)
+  while (true) {
+    // #@@range_begin(get_front_message)
+    __asm__("cli");
+    if (main_queue.Count() == 0) {
+      __asm__("sti\n\thlt");
+      continue;
+    }
+
+    Message msg = main_queue.Front();
+    main_queue.Pop();
+    __asm__("sti");
+    // #@@range_end(get_front_message)
+
+    switch (msg.type) {
+    case Message::kInterruptXHCI:
+      while (xhc.PrimaryEventRing()->HasFront()) {
+        if (auto err = ProcessEvent(xhc)) {
+          Log(kError, "Error while ProcessEvent: %s at %s:%d\n",
+              err.Name(), err.File(), err.Line());
+        }
+      }
+      break;
+    default:
+      Log(kError, "Unknown message type: %d\n", msg.type);
+    }
+  }
+  // #@@range_end(event_loop)
 }
 
 extern "C" void __cxa_pure_virtual() {
