5dec801e5b3c9c9cf06d4b47f76893e02e525004
song-dela-moon <song.dela.moon@gmail.com>
2026-02-07 15:40:35 +0900

    [KosmOS_day08a] Pass UEFI memory map to kernel
    Changelog
    ======
    - Define MemoryMap and MemoryDescriptor structures.
    - Update MikanLoader to retrieve and pass memory map to kernel.
    - Modify KernelMain signature to accept MemoryMap.
    
    Testing
    ======
    - Verified: Kernel successfully receives memory map from bootloader.
    
    Technical Insights
    ======
    - Transitioning from fixed memory layout to dynamic BIOS-provided topology.

--- KosmosLoaderPkg/Main.c
+++ KosmosLoaderPkg/Main.c
@@ -10,16 +10,8 @@
 #include  <Protocol/BlockIo.h>
 #include  <Guid/FileInfo.h>
 #include  "frame_buffer_config.hpp"
+#include  "memory_map.hpp"
 #include  "elf.hpp"
-
-struct MemoryMap {
-  UINTN buffer_size;
-  VOID* buffer;
-  UINTN map_size;
-  UINTN map_key;
-  UINTN descriptor_size;
-  UINT32 descriptor_version;
-};
 
 EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
   if (map->buffer == NULL) {
@@ -368,10 +360,12 @@ EFI_STATUS EFIAPI UefiMain(
       Halt();
   }
 
-  typedef void __attribute__((sysv_abi)) EntryPointType(const struct FrameBufferConfig*);
+  // #@@range_begin(pass_memory_map)
+  typedef void __attribute__((sysv_abi)) EntryPointType(const struct FrameBufferConfig*,
+                              const struct MemoryMap*);
   EntryPointType* entry_point = (EntryPointType*)entry_addr;
-  entry_point(&config);
-  // #@@range_end(pass_frame_buffer_config)
+  entry_point(&config, &memmap);
+  // #@@range_end(pass_memory_map)
 
   Print(L"All done\n");
 
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include "frame_buffer_config.hpp"
+#include "memory_map.hpp"
 #include "graphics.hpp"
 #include "mouse.hpp"
 #include "font.hpp"
@@ -103,7 +104,8 @@ void IntHandlerXHCI(InterruptFrame* frame) {
 // #@@range_end(xhci_handler)
 
 // #@@range_begin(call_pixel_writer)
-extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config,
+                           const MemoryMap& memory_map) {
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
       pixel_writer = new(pixel_writer_buf)
@@ -142,6 +144,31 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     printk("|__/  :__/ :______/ |_______/ |__/ |__/ |__/ :______/  :______/ \n");
   // #@@range_end(draw_desktop)
   SetLogLevel(kWarn);
+
+  const std::array available_memory_types{
+    MemoryType::kEfiBootServicesCode,
+    MemoryType::kEfiBootServicesData,
+    MemoryType::kEfiConventionalMemory,
+  };
+
+  // #@@range_begin(print_memory_map)
+  printk("memory_map: %p\n", &memory_map);
+  for (uintptr_t iter = reinterpret_cast<uintptr_t>(memory_map.buffer);
+       iter < reinterpret_cast<uintptr_t>(memory_map.buffer) + memory_map.map_size;
+       iter += memory_map.descriptor_size) {
+    auto desc = reinterpret_cast<MemoryDescriptor*>(iter);
+    for (int i = 0; i < available_memory_types.size(); ++i) {
+      if (desc->type == available_memory_types[i]) {
+        printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
+            desc->type,
+            desc->physical_start,
+            desc->physical_start + desc->number_of_pages * 4096 - 1,
+            desc->number_of_pages,
+            desc->attribute);
+      }
+    }
+  }
+  // #@@range_end(print_memory_map)
 
   // #@@range_begin(new_mouse_cursor)
   mouse_cursor = new(mouse_cursor_buf) MouseCursor{
